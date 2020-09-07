82cbfe92c92757c61721526265d10ae38a9ad9ec
gokul-sarath07 <gokul510.s@gmail.com>
2020-09-08 00:33:14 +0530

    Employee Check Added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,1 +1,12 @@
-// Employee Wage Computation Problem
+public class EmployeeWage {
+	public static void main(String[] args) {
+
+		int present = 1;
+		double valcheck=Math.floor(Math.random()*10)%2;
+
+		if (valcheck == present)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+}
