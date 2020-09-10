c27594d09a295dc21ab976fc685d4ab88a0ebf1c
gokul-sarath07 <gokul510.s@gmail.com>
2020-09-10 22:04:48 +0530

    Wage Computation Added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,18 @@
 public class EmployeeWage {
 	public static void main(String[] args) {
 
-		int present = 1;
-		double valcheck=Math.floor(Math.random()*10)%2;
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
 
-		if (valcheck == present)
-			System.out.println("Employee is Present");
+		int empHrs = 0;
+		int empWage = 0;
+
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
 		else
-			System.out.println("Employee is Absent");
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 }
