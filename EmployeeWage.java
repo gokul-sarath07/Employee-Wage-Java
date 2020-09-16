a50d8d6dbdab9903eb3f8d523846f149f6295be9
gokul-sarath07 <gokul510.s@gmail.com>
2020-09-16 22:58:56 +0530

    Multiple Companies Added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,20 +3,28 @@ public class EmployeeWage {
 	public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-	private final String company;
-	private final int empRatePerHrs;
-	private final int workingDaysPerMonth;
-	private final int maxHrsPerMonth;
-	private int totalEmpWage;
+	private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWageArray;
 
-	public void EmpWageBuilder(String company, int empRatePerHrs,
-                    int workingDaysPerMonth, int maxHrsPerMonth) {
-		this.company = company;
-		this.empRatePerHrs = empRatePerHrs;
-		this.workingDaysPerMonth = workingDaysPerMonth;
-		this.maxHrsPerMonth = maxHrsPerMonth;
+	public EmployeeWage() {
+		companyEmpWageArray =  new CompanyEmpWage[5];
+	}
 
-	public void computeEmpWage() {
+	private void addCompanyEmpWage(String company, int empRatePerHour,
+						int numOfWorkingDays, int maxHoursPerMonth) {
+		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHour,
+												numOfWorkingDays, maxHoursPerMonth);
+		numOfCompany++;
+	}
+
+	private int computeEmpWage() {
+		for (int i = 0; i < numOfCompany; i++) {
+			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
+
+	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
 
 		int empHrs = 0;
 		int totalEmpHrs = 0;
@@ -38,20 +46,13 @@ public class EmployeeWage {
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: "+ totalWorkingDays + " Emp Hr: " + empHrs);
 		}
-		totalEmpWage = totalEmpHrs * empRatePerHrs;
-	}
-
-	@Override
-	public String toString() {
-		return "Total Emp Wage for " + company + " is " + totalEmpWage;
+		return totalEmpHrs * companyEmpWage.empRatePerHrs;
 	}
 
 	public static void main(String[] args) {
-		EmpWageBuilder google = new EmpWageBuilder("Google", 20, 2, 10);
-		EmpWageBuilder facebook = new EmpWageBuilder("Facebook", 10, 6, 30);
-		google.computeEmpWage();
-		System.out.println("Google");
-		facebook.computeEmpWage();
-		System.out.println("Facebook");
+		EmployeeWage empWageBuilder = new EmployeeWage();
+		empWageBuilder.addCompanyEmpWage("Google", 20, 2, 10);
+		empWageBuilder.addCompanyEmpWage("Facebook", 10, 6, 30);
+		empWageBuilder.computeEmpWage();
 	}
 }
