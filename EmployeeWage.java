3671190058409a37a90fa3a33e73e85dfb02f280
gokul-sarath07 <gokul510.s@gmail.com>
2020-09-16 22:25:39 +0530

    Per Company Wage added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,8 +3,20 @@ public class EmployeeWage {
 	public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-	public static int computeEmpWage(String company, int empRatePerHrs,
-					int workingDaysPerMonth, int maxHrsPerMonth) {
+	private final String company;
+	private final int empRatePerHrs;
+	private final int workingDaysPerMonth;
+	private final int maxHrsPerMonth;
+	private int totalEmpWage;
+
+	public void EmpWageBuilder(String company, int empRatePerHrs,
+                    int workingDaysPerMonth, int maxHrsPerMonth) {
+		this.company = company;
+		this.empRatePerHrs = empRatePerHrs;
+		this.workingDaysPerMonth = workingDaysPerMonth;
+		this.maxHrsPerMonth = maxHrsPerMonth;
+
+	public void computeEmpWage() {
 
 		int empHrs = 0;
 		int totalEmpHrs = 0;
@@ -26,12 +38,20 @@ public class EmployeeWage {
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: "+ totalWorkingDays + " Emp Hr: " + empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * empRatePerHrs;
-		System.out.println("Total Emp Wage for " + company + " is " + totalEmpWage);
-		return totalEmpWage;
+		totalEmpWage = totalEmpHrs * empRatePerHrs;
 	}
+
+	@Override
+	public String toString() {
+		return "Total Emp Wage for " + company + " is " + totalEmpWage;
+	}
+
 	public static void main(String[] args) {
-		computeEmpWage("Google", 20, 2, 10);
-		computeEmpWage("Facebook", 10, 6, 30);
+		EmpWageBuilder google = new EmpWageBuilder("Google", 20, 2, 10);
+		EmpWageBuilder facebook = new EmpWageBuilder("Facebook", 10, 6, 30);
+		google.computeEmpWage();
+		System.out.println("Google");
+		facebook.computeEmpWage();
+		System.out.println("Facebook");
 	}
 }
